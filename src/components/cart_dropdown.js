3625c1019d6a6883dd17b5ba962da75658e1dd6c
gdamota <gdamota@sandiego.edu>
2021-06-10 10:37:29 -0400

    added remove item from cart

--- src/components/cart_dropdown.js
+++ src/components/cart_dropdown.js
@@ -3,10 +3,11 @@ import FancyButton from "./fancy_button";
 import CartItem from "./cart_item";
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
+import {toggleCartHidden} from "../redux/cart/cart_actions";
 
 import "./styles/cart_dropdown.scss";
 
-const Cart = ({cartItems, history}) => (
+const Cart = ({cartItems, history, dispatch}) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -15,7 +16,12 @@ const Cart = ({cartItems, history}) => (
         <span className="empty-message"> Your Cart is Empty</span>
       )}
     </div>
-    <FancyButton onClick={() => history.push("/checkout")}>
+    <FancyButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}
+    >
       CHECKOUT
     </FancyButton>
   </div>
