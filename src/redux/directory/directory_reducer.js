2e3c674145904dd5396a1d82dfe262c01172c1d3
gdamota <gdamota@sandiego.edu>
2021-07-01 12:43:38 -0400

    handling more routing + initial state

--- src/redux/directory/directory_reducer.js
+++ src/redux/directory/directory_reducer.js
@@ -1,7 +1,92 @@
 import {DirectoryActionTypes} from "./directory_types";
 
 const INITIAL_STATE = {
-  sections: []
+  sections: [
+    {
+      id: "88cb9fb7-285b-495b-b9d4-2027c01f3d97",
+      title: "Boutonnieres",
+      routeName: "boutonnieres",
+      items: [
+        {
+          id: "",
+          name: "",
+          imageUrl: "",
+          price: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      id: "d6f63ade-c560-477e-9989-85b1d204b50c",
+      title: "Bracelets",
+      routeName: "bracelets",
+      items: [
+        {
+          id: "",
+          name: "",
+          imageUrl: "",
+          price: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      id: "cfa85a57-44c2-4e1f-a1ce-2c7d7f244e43",
+      title: "Wearables",
+      routeName: "wearables",
+      items: [
+        {
+          id: "",
+          name: "",
+          imageUrl: "",
+          price: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      id: "6c798218-1945-4a2b-86e4-49ddd65cb80e",
+      title: "Necklaces",
+      routeName: "necklaces",
+      items: [
+        {
+          id: "",
+          name: "",
+          imageUrl: "",
+          price: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      id: "2a64e1db-3ea1-4438-9e6f-51017517442f",
+      title: "Earrings",
+      routeName: "earrings",
+      items: [
+        {
+          id: "",
+          name: "",
+          imageUrl: "",
+          price: "",
+          description: ""
+        }
+      ]
+    },
+    {
+      id: "fe1a274d-3460-4c3f-814c-6fd7a40c969d",
+      title: "Bouquets",
+      routeName: "bouquets",
+      items: [
+        {
+          id: "",
+          name: "",
+          imageUrl: "",
+          price: "",
+          description: ""
+        }
+      ]
+    }
+  ]
 };
 
 const directoryReducer = (state = INITIAL_STATE, action) => {
--- src/redux/shop/shop_selector.js
+++ src/redux/shop/shop_selector.js
@@ -5,7 +5,8 @@ const COLLECTION_ID_MAP = {
   boutonnieres: "88cb9fb7-285b-495b-b9d4-2027c01f3d97",
   bracelets: "d6f63ade-c560-477e-9989-85b1d204b50c",
   earrings: "2a64e1db-3ea1-4438-9e6f-51017517442f",
-  wear: 5
+  wearables: "cfa85a57-44c2-4e1f-a1ce-2c7d7f244e43",
+  necklaces: "6c798218-1945-4a2b-86e4-49ddd65cb80e"
 };
 
 const selectShop = state => state.directory;
