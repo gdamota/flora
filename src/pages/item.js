039251b12c2d8314052c827f59d0ce8a15b79c95
gdamota <gdamota@sandiego.edu>
2021-07-06 11:45:15 -0400

    added alot more product pics

--- src/pages/item.js
+++ src/pages/item.js
@@ -21,7 +21,12 @@ const ItemPage = ({item, shopActions, addItem, match}) => {
     <div className="item-page">
       <div className="item-header">
         <h2 className="title">{name.split("_").join(" ")}</h2>
-        <img src={process.env.PUBLIC_URL + imageUrl} alt={name} />
+        <img
+          src={process.env.PUBLIC_URL + imageUrl}
+          alt={name}
+          width="25%"
+          height="auto"
+        />
         <div className="item-description">
           <p className="description">{description}</p>
           <p className="price">{"Price: $" + price}</p>
