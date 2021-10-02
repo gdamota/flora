cba8513c50ef8f1199ec434acd294b6190184666
gdamota <gdamota@sandiego.edu>
2021-10-02 06:39:28 -0400

    added response paper after purchase along with email service to notify when purchase has been made

--- src/components/stripe_button.js
+++ src/components/stripe_button.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Paper from "@material-ui/core/Paper";
 import {createOrder} from "../graphql/mutations";
 import {API, Auth} from "aws-amplify";
+import emailjs from "emailjs-com";
 import {CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
 import "./styles/stripe.scss";
 
@@ -13,6 +14,7 @@ const StripeCheckoutForm = ({price, items}) => {
   let [address, setAddress] = useState();
   let [zip, setZip] = useState();
   let [state, setState] = useState();
+  let [confirmation, setConfirmation] = useState();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -38,87 +40,110 @@ const StripeCheckoutForm = ({price, items}) => {
       address: address,
       zip_code: zip,
       state: state,
-      products: items.map(item => ({
-        "product name": item.name,
-        quantity: item.quantity
-      })),
+      products: JSON.stringify(
+        items.map(item => ({
+          product_name: item.name,
+          quantity: item.quantity
+        }))
+      ),
       price: price,
       status: "unfiled",
       stripe_id: stripe_id
     };
-
     await API.graphql({
       query: createOrder,
       variables: {input: order}
     }).then(res => {
       console.log(res);
-      alert(
-        "Thank you for your purchase! We will email you when your item(s) ship with tracking information. Items usually ship within three business days."
-      );
+      setConfirmation("Thank You");
     });
+
+    try {
+      const templateParams = {
+        email: Auth.user.attributes.email,
+        message: JSON.stringify(order)
+      };
+
+      await emailjs.send(
+        "flora_order_confirmation",
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_USER_ID
+      );
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
     <Paper elevation={3}>
-      <form className="payment-form">
-        <h3>Shipping Information</h3>
-        <input
-          className="input"
-          type="text"
-          name="name"
-          placeholder="name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-        <input
-          className="input"
-          type="text"
-          name="address"
-          placeholder="address"
-          value={address}
-          onChange={e => setAddress(e.target.value)}
-        />
-        <input
-          className="input"
-          type="text"
-          name="zip"
-          placeholder="zip"
-          value={zip}
-          onChange={e => setZip(e.target.value)}
-        />
-        <input
-          className="input"
-          type="text"
-          name="state"
-          placeholder="state"
-          value={state}
-          onChange={e => setState(e.target.value)}
-        />
-        <h3>Card Information</h3>
-        <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: "14px",
-                color: "#424770",
-                "::placeholder": {
-                  color: "#aab7c4"
+      {confirmation ? (
+        <p>
+          Thank you for your purchase! We will email you when your item(s) ship
+          with the tracking information. Items usually ship within three to five
+          business days.
+        </p>
+      ) : (
+        <form className="payment-form">
+          <h3>Shipping Information</h3>
+          <input
+            className="input"
+            type="text"
+            name="name"
+            placeholder="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <input
+            className="input"
+            type="text"
+            name="address"
+            placeholder="address"
+            value={address}
+            onChange={e => setAddress(e.target.value)}
+          />
+          <input
+            className="input"
+            type="text"
+            name="zip"
+            placeholder="zip"
+            value={zip}
+            onChange={e => setZip(e.target.value)}
+          />
+          <input
+            className="input"
+            type="text"
+            name="state"
+            placeholder="state"
+            value={state}
+            onChange={e => setState(e.target.value)}
+          />
+          <h3>Card Information</h3>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "14px",
+                  color: "#424770",
+                  "::placeholder": {
+                    color: "#aab7c4"
+                  }
+                },
+                invalid: {
+                  color: "#9e2146"
                 }
-              },
-              invalid: {
-                color: "#9e2146"
               }
-            }
-          }}
-        />
-        <button
-          className="payment-button"
-          onClick={handleSubmit}
-          disabled={!address || !name || !zip || !state}
-        >
-          Pay
-        </button>
-      </form>
+            }}
+          />
+          <button
+            className="payment-button"
+            onClick={handleSubmit}
+            disabled={!address || !name || !zip || !state}
+          >
+            Pay
+          </button>
+        </form>
+      )}
     </Paper>
   );
 };
